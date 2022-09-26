58738d9c9fd4cc7fdc0e9beaafafc8eb8524155e
emre durgunlu <emredurgunlu@gmail.com>
2022-09-27 02:33:02 +0300

    third video - verification tests

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,5 +1,36 @@
 use super::*;
 pub struct Blockchain {
     pub blocks: Vec<Block>,
-    //unspent_outputs: HashSet<Hash>,
+}
+
+impl Blockchain {
+    pub fn verify(&self) -> bool {
+        for (i, block) in self.blocks.iter().enumerate() {
+            if block.index != i as u32 {//if the block index that the block claims to be located at is not where it actually is located, it fails the test
+                println!("Index mismatch {} != {}", &block.index, &i);
+                return false;
+            }else if !block::check_difficulty(&block.hash(), block.difficulty){
+                println!("Difficulty fail");
+                return false;
+            }else if i!=0 {
+                //not Genesis Block
+                let prev_block = &self.blocks[i-1];
+                if block.timestamp <= prev_block.timestamp {
+                    println!("Time did not increase");
+                    return false;
+                } else if block.prev_block_hash != prev_block.hash{
+                    println!("Hash mismatch");
+                    return false;
+                }
+            }else{
+                //Genesis Block
+                if block.prev_block_hash != vec![0;32]{
+                    println!("Genesis Block prev_block_hash invalid");
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
     let mut block = Block::new(
         0,
-        0,
+        now(),
         vec![0; 32],
         0,
         "Genesis block!".to_owned(),
@@ -21,14 +21,7 @@ fn main() {
     };
 
     for i in 1..=10 {
-        let mut block = Block::new(
-            i,
-            0,
-            last_hash,
-            0,
-            "Another block".to_owned(),
-            difficulty,
-        );
+        let mut block = Block::new(i, now(), last_hash, 0, "Another block".to_owned(), difficulty);
 
         block.mine();
         println!("Mined genesis block {:?}", &block);
@@ -36,5 +29,12 @@ fn main() {
         last_hash = block.hash.clone();
 
         blockchain.blocks.push(block);
+
+        println!("Verify {}", &blockchain.verify());
     }
+        //blockchain.blocks[3].index = 4;//output: Index mismatch 4 != 3 Verify false
+        //blockchain.blocks[3].hash[8] += 1;//output: Hash mismatch 4 != 3 Verify false
+        //blockchain.blocks[3].payload = "Nope".to_owned();//output: Difficulty fail Verify false
+        //blockchain.blocks[3].prev_block_hash[18] = 8;//output: Difficulty fail Verify false
+        //println!("Verify {}", &blockchain.verify());
 }
